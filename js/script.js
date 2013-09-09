303bcd468e43739f5885e3c66841806cb847a0ec
Omid Ghomeshi <omidghomeshi@gmail.com>
2013-09-09 01:41:21 -0400

    Added bootstrap script.js correctly works index.html fixed

--- js/script.js
+++ js/script.js
@@ -1,23 +1,35 @@
+
+//Capitlizes first letter of first name and last name
 String.prototype.toCap = function() {
 	var string = this.toString();
 	if(!string.length) return;
-	return string[0].toUpperCase() + string.slice(1);
+	var lastName = string.split(' ').pop();
+	var firstName = string.split(' ')[0];
+	lastName = lastName[0].toUpperCase() + lastName.slice(1);
+	firstName = firstName[0].toUpperCase() + firstName.slice(1);
+	string = firstName + " " + lastName;
+	return string;
 };
-
-console.log("test".toCap());
 
 $(document).ready(function() {
 	$('#submit').click(function(e) {
 
 		e.preventDefault();
 
-		var userData = {};
+		var userData = {
+			interests: []
+		};
 
-		userData.name = $('#fname').val().toCap() + " " + $('#lname').val().toCap();
+		userData.name = $('#name').val().toCap();
 		userData.email = $('#uniqname').val() + "@umich.edu";;
-		userData.year = $('#year').val();
-		userData.major = $('#major').val();
- 
+		userData.year = $('#yearSelect').val();
+		userData.major = $('#majorSelect').val();
+
+		$("input:checkbox:checked").each(function() {
+       		userData.interests.push($(this).val());
+  		});
+ 		
+ 		console.log(userData.interests);
 		console.log(userData);
 
 	})
